c8b695100606fee903e9f81d25a80b2f6d334013
Andrew <49383564+andrewc910@users.noreply.github.com>
2021-01-14 14:20:50 -0500

    Lazy load big images (#1)
    * lazy load images
    
    * Working on optimizing lazy load
    
    * compress gif
    
    * Converted gif to webm for better compression, not in use though as page size seems fine for now

--- components/home/BridgeCaseStudy.js
+++ components/home/BridgeCaseStudy.js
@@ -1,24 +1,36 @@
+import LazyLoad from 'react-lazyload';
+
 export default function BridgeCaseStudy() {
   return (
     <section className="bg-gray-100">
       <div className="flex flex-1 justify-around flex-col lg:flex-row">
         <div className="lg:mr-4 lg:m-10 m-4 sm:mb-0 flex justify-center">
           <div className="max-w-2xl">
-            <img
-              alt="bridgeapi logo"
-              src="/assets/images/bridge_logo.svg"
-              className="md:max-w-md self-center lg:hidden mx-auto mb-4"
-            />
-            <img alt="create bridge gif" src="/assets/gifs/create_bridge.gif" />
+            <LazyLoad offset={100} once>
+              <img
+                alt="bridgeapi logo"
+                src="/assets/images/bridge_logo.svg"
+                className="md:max-w-md self-center lg:hidden mx-auto mb-4"
+              />
+
+              <img
+                alt="create bridge gif"
+                src="/assets/gifs/bridge_min.gif"
+              />
+            </LazyLoad>
           </div>
         </div>
 
         <div className="sm:mt-0 lg:text-left lg:m-10 lg:ml-4 mx-4 mb-4 flex flex-col">
-          <img
-            alt="bridgeapi logo"
-            src="/assets/images/bridge_logo.svg"
-            className="md:max-w-md self-center hidden lg:block"
-          />
+          <LazyLoad offset={100} once>
+            <div className="flex flex-col">
+              <img
+                alt="bridgeapi logo"
+                src="/assets/images/bridge_logo.svg"
+                className="md:max-w-md self-center hidden lg:block"
+              />
+            </div>
+          </LazyLoad>
           <p
             className="mt-3 mb-4 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 text-center"
           >
